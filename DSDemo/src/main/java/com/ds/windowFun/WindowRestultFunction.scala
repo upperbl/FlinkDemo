0d1af808ca853b6594b89f4ab7493e3b6852c2c6
upperbl <upperbl@qq.com>
2020-07-08 14:33:34 +0800

    [Change project name to DSDEMO][02]

---
+++ DSDemo/src/main/java/com/ds/windowFun/WindowRestultFunction.scala
@@ -0,0 +1,10 @@
+package com.ds.windowFun
+import com.ds.protocol.ItemViewCount
+import org.apache.flink.streaming.api.scala.function.WindowFunction
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
+class WindowRestultFunction extends WindowFunction[Long,ItemViewCount,Long,TimeWindow]{
+  override def apply(key: Long, window: TimeWindow, input: Iterable[Long], out: Collector[ItemViewCount]): Unit = {
+    out.collect(ItemViewCount(key,window.getEnd,input.iterator.next()))
+  }
+}
